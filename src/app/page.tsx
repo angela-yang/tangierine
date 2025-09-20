a02a0ceb79189e4cdcda043102805171cd6261da
Angela <angela.xq.yang@gmail.com>
2025-09-19 23:50:39 -0700

    Added favicon and title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <div className="w-full h-screen bg-cover bg-center" style={{ backgroundImage: "url('/images/bg.png')" }}>
       <HomeNav />
       <ShopScene />
+      <link rel="icon" href="/images/orange.png" type="image/png" sizes="any" />
     </div>
   )
 }
